4d0ef2be9ef80c0b6671a21c6272c620d31dc623
frozein <elwell.daniel.2@gmail.com>
2025-02-10 12:36:31 -0500

    fixed build error

--- splv/include/spatialstudio/splv_buffer_io.h
+++ splv/include/spatialstudio/splv_buffer_io.h
@@ -40,110 +40,43 @@ typedef struct SPLVbufferWriter
 /**
  * initializes a buffer reader
  */
-SPLV_NOMANGLE SPLVerror splv_buffer_reader_create(SPLVbufferReader* reader, uint8_t* buf, uint64_t len);
+SPLV_API SPLVerror splv_buffer_reader_create(SPLVbufferReader* reader, uint8_t* buf, uint64_t len);
 
 /**
  * reads from a buffer reader
  */
-SPLV_NOMANGLE inline SPLVerror splv_buffer_reader_read(SPLVbufferReader* reader, uint64_t size, void* dst)
-{
-	if(size + reader->readPos > reader->len)
-	{
-		SPLV_LOG_ERROR("trying to read past end of buffer");
-		return SPLV_ERROR_FILE_READ;
-	}
-
-	memcpy(dst, &reader->buf[reader->readPos], size);
-	reader->readPos += size;
-
-	return SPLV_SUCCESS;
-}
+SPLV_API inline SPLVerror splv_buffer_reader_read(SPLVbufferReader* reader, uint64_t size, void* dst);
 
 /**
  * sets the read position for a buffer reader
  */
-SPLV_NOMANGLE inline SPLVerror splv_buffer_reader_seek(SPLVbufferReader* reader, uint64_t pos)
-{
-	if(pos >= reader->len)
-	{
-		SPLV_LOG_ERROR("trying to seek past end of buffer");
-		return SPLV_ERROR_FILE_READ;
-	}
-
-	reader->readPos = pos;
-	
-	return SPLV_SUCCESS;
-}
+SPLV_API inline SPLVerror splv_buffer_reader_seek(SPLVbufferReader* reader, uint64_t pos);
 
 //-------------------------------------------//
 
 /**
  * initalizes a buffer writer, must call splv_buffer_writer_destroy() when finished to free memory
  */
-SPLV_NOMANGLE SPLVerror splv_buffer_writer_create(SPLVbufferWriter* writer, uint64_t initialLen);
+SPLV_API SPLVerror splv_buffer_writer_create(SPLVbufferWriter* writer, uint64_t initialLen);
 
 /**
  * destroys a buffer writer, frees resources allocated from splv_buffer_writer_create()
  */
-SPLV_NOMANGLE inline void splv_buffer_writer_destroy(SPLVbufferWriter* writer)
-{
-	if(writer->buf)
-		SPLV_FREE(writer->buf);
-}
+SPLV_API inline void splv_buffer_writer_destroy(SPLVbufferWriter* writer);
 
 /**
  * writes to a buffer writer
  */
-SPLV_NOMANGLE inline SPLVerror splv_buffer_writer_write(SPLVbufferWriter* writer, uint64_t size, void* src)
-{
-	if(writer->writePos + size > writer->len)
-	{
-		uint64_t newLen = writer->len;
-		while(writer->writePos + size > newLen)
-			newLen *= 2;
+SPLV_API inline SPLVerror splv_buffer_writer_write(SPLVbufferWriter* writer, uint64_t size, void* src);
 
-		uint8_t* newBuf = (uint8_t*)SPLV_REALLOC(writer->buf, newLen);
-		if(!newBuf)
-		{
-			SPLV_LOG_ERROR("failed to realloc buffer to write to");
-			return SPLV_ERROR_OUT_OF_MEMORY;
-		}
+/**
+ * writes a single byte to a writer
+ */
+SPLV_API inline SPLVerror splv_buffer_writer_put(SPLVbufferWriter* writer, uint8_t c);
 
-		writer->buf = newBuf;
-		writer->len = newLen;
-	}
-
-	memcpy(writer->buf + writer->writePos, src, size);
-	writer->writePos += size;
-
-	return SPLV_SUCCESS;
-}
-
-SPLV_NOMANGLE inline SPLVerror splv_buffer_writer_put(SPLVbufferWriter* writer, uint8_t c)
-{
-	if(writer->writePos + 1 > writer->len)
-	{
-		uint64_t newLen = writer->len * 2;
-		uint8_t* newBuf = (uint8_t*)SPLV_REALLOC(writer->buf, newLen);
-		if(!newBuf)
-		{
-			SPLV_LOG_ERROR("failed to realloc buffer to write to");
-			return SPLV_ERROR_OUT_OF_MEMORY;
-		}
-
-		writer->buf = newBuf;
-		writer->len = newLen;
-	}
-
-	writer->buf[writer->writePos] = c;
-	writer->writePos++;
-
-	return SPLV_SUCCESS;
-}
-
-SPLV_NOMANGLE inline void splv_buffer_writer_reset(SPLVbufferWriter* writer)
-{
-	writer->writePos = 0;
-}
+/**
+ * reset's a writers write position without freeing its memory
+ */
+SPLV_API inline void splv_buffer_writer_reset(SPLVbufferWriter* writer);
 
 #endif //#ifndef SPLV_BUFFER_IO_H
--- splv/src/splv_buffer_io.c
+++ splv/src/splv_buffer_io.c
@@ -11,6 +11,36 @@ SPLVerror splv_buffer_reader_create(SPLVbufferReader* reader, uint8_t* buf, uint
 	return SPLV_SUCCESS;
 }
 
+SPLVerror splv_buffer_reader_read(SPLVbufferReader* reader, uint64_t size, void* dst)
+{
+	if(size + reader->readPos > reader->len)
+	{
+		SPLV_LOG_ERROR("trying to read past end of buffer");
+		return SPLV_ERROR_FILE_READ;
+	}
+
+	memcpy(dst, &reader->buf[reader->readPos], size);
+	reader->readPos += size;
+
+	return SPLV_SUCCESS;
+}
+
+SPLVerror splv_buffer_reader_seek(SPLVbufferReader* reader, uint64_t pos)
+{
+	if(pos >= reader->len)
+	{
+		SPLV_LOG_ERROR("trying to seek past end of buffer");
+		return SPLV_ERROR_FILE_READ;
+	}
+
+	reader->readPos = pos;
+	
+	return SPLV_SUCCESS;
+}
+
+
+//-------------------------------------------//
+
 SPLVerror splv_buffer_writer_create(SPLVbufferWriter* writer, uint64_t initialLen)
 {
 	if(initialLen == 0)
@@ -30,4 +60,62 @@ SPLVerror splv_buffer_writer_create(SPLVbufferWriter* writer, uint64_t initialLe
 	writer->writePos = 0;
 
 	return SPLV_SUCCESS;
+}
+
+void splv_buffer_writer_destroy(SPLVbufferWriter* writer)
+{
+	if(writer->buf)
+		SPLV_FREE(writer->buf);
+}
+
+SPLVerror splv_buffer_writer_write(SPLVbufferWriter* writer, uint64_t size, void* src)
+{
+	if(writer->writePos + size > writer->len)
+	{
+		uint64_t newLen = writer->len;
+		while(writer->writePos + size > newLen)
+			newLen *= 2;
+
+		uint8_t* newBuf = (uint8_t*)SPLV_REALLOC(writer->buf, newLen);
+		if(!newBuf)
+		{
+			SPLV_LOG_ERROR("failed to realloc buffer to write to");
+			return SPLV_ERROR_OUT_OF_MEMORY;
+		}
+
+		writer->buf = newBuf;
+		writer->len = newLen;
+	}
+
+	memcpy(writer->buf + writer->writePos, src, size);
+	writer->writePos += size;
+
+	return SPLV_SUCCESS;
+}
+
+SPLVerror splv_buffer_writer_put(SPLVbufferWriter* writer, uint8_t c)
+{
+	if(writer->writePos + 1 > writer->len)
+	{
+		uint64_t newLen = writer->len * 2;
+		uint8_t* newBuf = (uint8_t*)SPLV_REALLOC(writer->buf, newLen);
+		if(!newBuf)
+		{
+			SPLV_LOG_ERROR("failed to realloc buffer to write to");
+			return SPLV_ERROR_OUT_OF_MEMORY;
+		}
+
+		writer->buf = newBuf;
+		writer->len = newLen;
+	}
+
+	writer->buf[writer->writePos] = c;
+	writer->writePos++;
+
+	return SPLV_SUCCESS;
+}
+
+void splv_buffer_writer_reset(SPLVbufferWriter* writer)
+{
+	writer->writePos = 0;
 }
